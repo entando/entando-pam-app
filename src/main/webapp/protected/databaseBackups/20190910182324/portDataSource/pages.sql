7fa3f838b60dffd226ec50eca6bf078cf17e00b4
joewhite101 <j.white@entando.com>
2019-09-10 18:24:02 +0200

    New backup

---
+++ src/main/webapp/protected/databaseBackups/20190910182324/portDataSource/pages.sql
@@ -0,0 +1,11 @@
+INSERT INTO pages (code,parentcode,pos) VALUES ('service','homepage',1);
+INSERT INTO pages (code,parentcode,pos) VALUES ('login','service',3);
+INSERT INTO pages (code,parentcode,pos) VALUES ('notfound','service',1);
+INSERT INTO pages (code,parentcode,pos) VALUES ('errorpage','service',2);
+INSERT INTO pages (code,parentcode,pos) VALUES ('homepage','homepage',-1);
+INSERT INTO pages (code,parentcode,pos) VALUES ('case_widgets','homepage',2);
+INSERT INTO pages (code,parentcode,pos) VALUES ('task_details','homepage',3);
+INSERT INTO pages (code,parentcode,pos) VALUES ('new_process','homepage',4);
+INSERT INTO pages (code,parentcode,pos) VALUES ('task_list','homepage',5);
+INSERT INTO pages (code,parentcode,pos) VALUES ('ftm','homepage',6);
+INSERT INTO pages (code,parentcode,pos) VALUES ('authhome','homepage',7);
